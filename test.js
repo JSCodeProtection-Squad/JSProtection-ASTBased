6e9cf6fd8cc15f061dd1cff28af9f8d2d31c057b
Fangkui Xie <xiefangkui@outlook.com>
2018-07-06 10:04:50 +0800

    just for syc

--- test.js
+++ test.js
@@ -1,10 +1,20 @@
 var numeric =  require('numeric')
-var N =10;
+var n =10;
 var A = [[1,2],
     [1,1]];
-
-var xy = numeric.random([2,10])
-console.log(xy)
+var m = n/3
+var N = numeric.round(numeric.dot(numeric.round([m]),numeric.random([1, n])));
+console.log(N);
+var xy = numeric.random([2,10]);
+console.log("xy============================");
+console.log(xy);
 out = numeric.mod(numeric.dot(A, xy), 1);
+console.log("M============================");
 console.log(out);
+// var _N = numeric.round(numeric)
+
+console.log(m);
+console.log("_N============================");
+var _N = numeric.round(numeric.dot(numeric.transpose(out),[[m/2.0],[m/2.0]]));
+console.log(_N);
 
